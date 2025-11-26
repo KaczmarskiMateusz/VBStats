2b24622eb398962a7263c3601ac6aae1f72ba280
mati <kaczmarskim95@gmail.com>
2025-11-26 18:25:25 +0100

    fix admin account migration

--- docker/db/migration/V1__add_admin_account.sql
+++ docker/db/migration/V1__add_admin_account.sql
@@ -3,10 +3,10 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@vbs.com') THEN
         INSERT INTO app.users (external_id, email, username, phone, active, confirmed, role, password)
         VALUES (
-          '00000000-0000-0000-0000-000000000001',
+          '5ac873ad-4902-4115-a649-f4ed683af167',
           'admin@vbs.com',
           'admin',
-          '000000000',
+          '123456789',
           true,
           true,
           'ADMIN',
