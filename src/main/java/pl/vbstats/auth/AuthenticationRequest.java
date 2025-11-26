b7fdd73715426d8c16406e4a5e3c19841c90769b
mati <kaczmarskim95@gmail.com>
2025-11-26 18:21:56 +0100

    change login email or username

--- src/main/java/pl/vbstats/auth/AuthenticationRequest.java
+++ src/main/java/pl/vbstats/auth/AuthenticationRequest.java
@@ -1,9 +1,11 @@
 package pl.vbstats.auth;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record AuthenticationRequest(
-        @NotBlank(message = "login cannot be blank")
+        @NotBlank(message = "login  cannot be blank")
         String login,
         @NotBlank(message = "password cannot be blank")
         String password) {
--- src/main/java/pl/vbstats/config/AuthenticationResponse.java
+++ src/main/java/pl/vbstats/config/AuthenticationResponse.java
@@ -4,14 +4,11 @@ import lombok.Builder;
 import lombok.Data;
 import pl.vbstats.user.Model.UserDto;
 
-import java.util.UUID;
-
 @Data
 @Builder
 public class AuthenticationResponse {
 
     private String token;
     private UserDto user;
-    private UUID externalId;
 
 }
